c62997e43d3df7717a85b02baabc76528b669dd3
noamyft <noamyft@gmail.com>
2019-01-27 02:26:03 +0200

    mutate_methods.py: kill processes after timeout

--- mutate_methods.py
+++ mutate_methods.py
@@ -1,6 +1,9 @@
 import multiprocessing as mp
+import subprocess
 import os
 import sys
+import shutil
+import time
 
 
 MUTATOR="target/cug-1.0-SNAPSHOT.jar"
@@ -8,15 +11,28 @@ NUM_THREADS=1
 MUTATION_LEVEL = sys.argv[2]
 REMOVE="rm"
 # REMOVE="del"
+TIME_TO_TIMEOUT = 300
 
-def mutate_file(path:str):
+mypath = sys.argv[1]
 
-    os.system("java -cp " + MUTATOR + " Main --mutate_methods --file " + path + " --mutation_level " + MUTATION_LEVEL + " --num_threads "  + str(NUM_THREADS))
+def mutate_file(filename:str):
+
+	path = os.path.join(mypath, filename).replace("\\", "/")
+	p = subprocess.Popen(["java", "-cp", MUTATOR, "Main", "--mutate_methods", "--file", path, "--mutation_level", MUTATION_LEVEL, "--num_threads", str(NUM_THREADS)])
+	time.sleep(TIME_TO_TIMEOUT)
+	poll = p.poll()
+	if poll is None:
+		print("TIMEOUT: ",path)
+		p.kill()
+		shutil.move(path, os.path.join("timeout", filename))
+		
 
 if __name__ == "__main__":
 
-    mypath = sys.argv[1]
-    alldirs = [os.path.join(mypath, f).replace("\\", "/") for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
+    if not os.path.exists("timeout"):
+        os.makedirs("timeout")
+    
+    alldirs = [ f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
 
-    pool = mp.Pool(processes=mp.cpu_count())
+    pool = mp.Pool(processes=1)#mp.cpu_count())
     pool.map(mutate_file, alldirs)
