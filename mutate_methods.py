580eb6ee20eda69d6f3d89fdc4ebb26bae305103
noamyft <noamyft@gmail.com>
2019-01-27 04:03:02 +0200

    mutate_methods.py: use timer to kill mutations

--- mutate_methods.py
+++ mutate_methods.py
@@ -4,6 +4,8 @@ import os
 import sys
 import shutil
 import time
+from threading import Timer
+import signal
 
 
 MUTATOR="target/cug-1.0-SNAPSHOT.jar"
@@ -11,20 +13,29 @@ NUM_THREADS=1
 MUTATION_LEVEL = sys.argv[2]
 REMOVE="rm"
 # REMOVE="del"
-TIME_TO_TIMEOUT = 300
+TIME_TO_TIMEOUT = 2
 
 mypath = sys.argv[1]
 
 def mutate_file(filename:str):
 
+	def timer_and_kill(p):
+		poll = p.poll()
+		if poll is None:
+			print("TIMEOUT: ",path)
+			os.kill(p.pid, signal.SIGKILL)
+			shutil.move(path, os.path.join("timeout", filename))
+	
 	path = os.path.join(mypath, filename).replace("\\", "/")
 	p = subprocess.Popen(["java", "-cp", MUTATOR, "Main", "--mutate_methods", "--file", path, "--mutation_level", MUTATION_LEVEL, "--num_threads", str(NUM_THREADS)])
-	time.sleep(TIME_TO_TIMEOUT)
-	poll = p.poll()
-	if poll is None:
-		print("TIMEOUT: ",path)
-		p.kill()
-		shutil.move(path, os.path.join("timeout", filename))
+	timer = Timer(TIME_TO_TIMEOUT, timer_and_kill, [p])
+	
+	try:
+		timer.start()
+		stdout, stderr = p.communicate()
+	finally:
+		timer.cancel()
+	
 		
 
 if __name__ == "__main__":
@@ -34,5 +45,5 @@ if __name__ == "__main__":
     
     alldirs = [ f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
 
-    pool = mp.Pool(processes=1)#mp.cpu_count())
+    pool = mp.Pool(processes=mp.cpu_count())
     pool.map(mutate_file, alldirs)
